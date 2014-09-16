491facc161379d86c011967275122b5a8aa8f4d0
Pahud <maximepahud@live.fr>
2014-09-16 12:07:28 +0200

    NewConverters Color dataparsedview

---
+++ Source/NavRTK.ModuleGPS/Converters/NSatBackgroundToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace NavRTK.ModuleGPS.Converters
+{
+    class NSatBackgroundToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int state = int.Parse(value.ToString());
+
+            if (state >= 6)
+                return new SolidColorBrush(Color.FromRgb(0, 67, 11)); // green dark
+            else if (state == 5 )
+                return new SolidColorBrush(Color.FromRgb(102, 66, 0)); // orange dark
+
+            else return new SolidColorBrush(Color.FromRgb(91, 0, 0)); // red dark
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Source/NavRTK.ModuleGPS/Converters/NSatToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace NavRTK.ModuleGPS.Converters
+{
+    class NSatToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int state = int.Parse(value.ToString());
+
+            if (state >= 6)
+                return new SolidColorBrush(Color.FromRgb(0, 135, 22)); // green light
+            else if (state == 5 )
+                return new SolidColorBrush(Color.FromRgb(255, 165, 0)); // orange light
+
+            else return new SolidColorBrush(Color.FromRgb(229, 0, 0)); // red light
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
